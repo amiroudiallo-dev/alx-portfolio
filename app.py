4b5b966b64ca1b0e4a8cd8feb9817c82b3d1467a
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-05-29 11:28:00 +0000

    create-app.py

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-	retrun "Hello world!";
-
+    return "Hello, World!"
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
